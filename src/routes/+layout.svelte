0d540a5760a7fb04748e237e7a1fe950d7a84261
K400000 <enu.cypher@gmail.com>
2026-06-08 15:32:16 +0700

    change favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="https://admissions.src.ku.ac.th/assets/img/faviconx.png" />
 </svelte:head>
 
 {@render children()}
